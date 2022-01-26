e2852c910d07971276c6e4e191a968d890b31d7c
liana <mustafaevaliana779@gmail.com>
2022-01-26 17:23:42 +0300

    Initial commit add LoginFragment

---
+++ app/src/main/java/com/bunbeauty/notes/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bunbeauty.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+*1.Single Activity
+* */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ app/src/main/java/com/bunbeauty/notes/ui/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.bunbeauty.notes.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bunbeauty.notes.R
+class LoginFragment : Fragment() {
+
+
+}
