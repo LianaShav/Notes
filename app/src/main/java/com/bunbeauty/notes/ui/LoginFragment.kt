7984d92af3832c029c9bcc25e0b3c4491b8105de
liana <mustafaevaliana779@gmail.com>
2022-01-31 17:03:34 +0300

    Add ViewBinding

--- app/src/main/java/com/bunbeauty/notes/ui/LoginFragment.kt
+++ app/src/main/java/com/bunbeauty/notes/ui/LoginFragment.kt
@@ -1,12 +1,14 @@
 package com.bunbeauty.notes.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.bunbeauty.notes.R
+import androidx.fragment.app.Fragment
+import com.bunbeauty.notes.databinding.FragmentLoginBinding
+
 class LoginFragment : Fragment() {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
 }
