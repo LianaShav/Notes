404d5a05a3172cdf04c4ae208b0d7b8fd0d98fb2
liana <mustafaevaliana779@gmail.com>
2022-02-12 23:27:11 +0300

    Add FragmentContainer,Create Binding in BaseFragment.kt

--- app/src/main/java/com/bunbeauty/notes/MainActivity.kt
+++ app/src/main/java/com/bunbeauty/notes/MainActivity.kt
@@ -2,13 +2,19 @@ package com.bunbeauty.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bunbeauty.notes.databinding.ActivityMainBinding
+
 /*
 *1.Single Activity
 * */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
     }
 
 }
--- app/src/main/java/com/bunbeauty/notes/ui/base/BaseFragment.kt
+++ app/src/main/java/com/bunbeauty/notes/ui/base/BaseFragment.kt
@@ -1,8 +1,41 @@
 package com.bunbeauty.notes.ui.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
 
 abstract class BaseFragment<B : ViewBinding> : Fragment() {
-    var fragment:String=""
+    var fragment: String = ""
+    private var mutableBinding: B? = null
+    val binding: B
+        get() = checkNotNull(mutableBinding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mutableBinding = createBindingInstance(inflater, container)
+        return mutableBinding?.root
+
+    }
+    @Suppress("UNCHECKED_CAST")
+    protected open fun createBindingInstance(inflater: LayoutInflater, container: ViewGroup?): B {
+        val viewBindingClass =
+            (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<B>
+        val method = viewBindingClass.getMethod(
+            "inflate",
+            LayoutInflater::class.java,
+            ViewGroup::class.java,
+            Boolean::class.java
+        )
+
+        return method.invoke(null, inflater, container, false) as B
+    }
+
 }
